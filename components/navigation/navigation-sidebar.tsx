4fcd3839f74cb4ab2be14a363843ed04e47cf671
Gustavo <gustavo@invera.com.ar>
2023-10-09 23:57:23 -0300

    Add: Navigation Action

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,7 +1,8 @@
 import { currentProfile } from '@/lib/current-profile'
 import { db } from '@/lib/db'
 import { redirect } from 'next/navigation'
-import React from 'react'
+
+import NavigationAction from './navigation-action'
 
 export const NavigationSidebar = async () => {
   const profile = await currentProfile()
@@ -21,7 +22,7 @@ export const NavigationSidebar = async () => {
   })
   return (
     <div className='space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3'>
-      Navigation Sidebar
+      <NavigationAction />
     </div>
   )
 }
