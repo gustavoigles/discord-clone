2cff251c8c10b3357ed6f1abe03158ac5839b6e7
Gustavo <gustavo@invera.com.ar>
2023-10-14 15:30:23 -0300

    Add: Create Server Modal & NavigationItem

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,26 +1,26 @@
-import InitialModal from "@/components/modals/initial-modal";
-import { db } from "@/lib/db";
-import { initialProfile } from "@/lib/initial-profile";
-import { redirect } from "next/navigation";
+import InitialModal from '@/components/modals/initial-modal'
+import { db } from '@/lib/db'
+import { initialProfile } from '@/lib/initial-profile'
+import { redirect } from 'next/navigation'
 
 const SetupPage = async () => {
-    const profile = await initialProfile();
+  const profile = await initialProfile()
 
-    const server = await db.server.findFirst({
-        where: {
-            members: {
-                some: {
-                    profileId: profile.id,
-                }
-            }
-        },
-    });
-
-    if (server) {
-        return redirect(`/servers/${server.id}`)
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id
+        }
+      }
     }
+  })
 
-    return <InitialModal />
-};
+  if (server) {
+    return redirect(`/servers/${server.id}`)
+  }
 
-export default SetupPage;
+  return <InitialModal open={true} />
+}
+
+export default SetupPage
--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -2,8 +2,12 @@ import { currentProfile } from '@/lib/current-profile'
 import { db } from '@/lib/db'
 import { redirect } from 'next/navigation'
 import { Separator } from '@/components/ui/separator'
+import { ScrollArea } from '@/components/ui/scroll-area'
 
-import NavigationAction from './navigation-action'
+import { NavigationAction } from './navigation-action'
+import { NavigationItem } from './navigation-item'
+import { ModeToggle } from '../mode-toggle'
+import { UserButton } from '@clerk/nextjs'
 
 export const NavigationSidebar = async () => {
   const profile = await currentProfile()
@@ -25,7 +29,29 @@ export const NavigationSidebar = async () => {
   return (
     <div className='space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3'>
       <NavigationAction />
-      <Separator className='h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto' />
+      <Separator className='px-3' />
+      <ScrollArea className='flex-1 w-full'>
+        {servers.map(server => (
+          <div key={server.id} className='mb-4'>
+            <NavigationItem
+              id={server.id}
+              name={server.name}
+              imageUrl={server.imageUrl}
+            />
+          </div>
+        ))}
+      </ScrollArea>
+      <div className='pb-3 mt-auto flex items-center flex-col gap-y-4'>
+        <ModeToggle />
+        <UserButton
+          afterSignOutUrl='/'
+          appearance={{
+            elements: {
+              avatarBox: 'h-[48px] w-[48px]'
+            }
+          }}
+        />
+      </div>
     </div>
   )
 }
